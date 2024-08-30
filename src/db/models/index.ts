2b378301b0e547d88e89f601b43f8b3ba6a15c8f
Jacky Tea <44061647+JackyTea@users.noreply.github.com>
2024-08-30 19:57:05 -0400

    chore: remove init models temporarily

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -1,33 +1,3 @@
-import { readdirSync } from 'fs'
-import { basename, dirname, join } from 'path'
-
-const __filename = 'src/db/models/index.ts'
-
-const __dirname = dirname(__filename)
-
-const models: { [key: string]: any } = {}
-const modelsDir = __dirname
-
-async function initializeModels() {
-  const files = readdirSync(modelsDir).filter((file) => {
-    return file.endsWith('.ts') && file !== basename(__filename)
-  })
-
-  for (const file of files) {
-    const model = await import(join(modelsDir, file))
-    const modelName = Object.keys(model)[0]
-    models[modelName] = model[modelName]
-  }
-
-  for (const modelName in models) {
-    if (models[modelName].associate) {
-      models[modelName].associate(models)
-    }
-  }
-}
-
-initializeModels()
-
 export { Contact } from '@/db/models/contact.ts'
 export { Partner } from '@/db/models/partner.ts'
 export { Member } from '@/db/models/member.ts'
